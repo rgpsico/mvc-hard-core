00e012962d38b40b7fe8bb65e23c7b9bd8460453
rgpsico <gerro121@hotmail.com>
2022-03-13 12:11:18 -0300

    Aplicando migration

---
+++ core/Database.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace app\core;
+
+class Database
+{
+
+    public \PDO $pdo;
+
+    public function __construct(array $config)
+    {
+        $dns = $config['dsn'] ?? '';
+        $user = $config['user'] ?? '';
+        $password = $config['password'] ?? '';
+        $this->pdo = new \PDO($dns, $user, $password);
+        $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+    }
+
+    public function applyMigrations()
+    {
+        $this->createMigrationsTable();
+        $appliedMigrations = $this->getAppliedMigrations();
+
+
+        $newMigrations = [];
+
+        $files = scandir(Application::$ROOT_DIR . '/migrations');
+        $toApplyMigrations = array_diff($files, $appliedMigrations);
+        foreach ($toApplyMigrations as $migration) {
+            if ($migration === '.' || $migration === '..') {
+                continue;
+            }
+
+
+            require_once Application::$ROOT_DIR . '/migrations/' . $migration;
+            $className = pathinfo($migration, PATHINFO_FILENAME);
+            $this->log("Aplicando migration $migration");
+            $instance = new $className();
+            $instance->up();
+            $this->log("A $migration migrate foi aplicada");
+            $newMigrations[] = $migration;
+        }
+
+        if (!empty($newMigrations)) {
+            $this->saveMigrations($newMigrations);
+        } else {
+            $this->log("Todas as migrations ja foram aplicadas");
+        }
+    }
+
+    public function createMigrationsTable()
+    {
+        $this->pdo->exec("CREATE TABLE IF NOT EXISTS migrations (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            migration VARCHAR(255),
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )  ENGINE=INNODB;");
+    }
+
+    public function getAppliedMigrations()
+    {
+        $statement = $this->pdo->prepare("SELECT migration FROM migrations");
+        $statement->execute();
+
+        return $statement->fetchAll(\PDO::FETCH_COLUMN);
+    }
+
+    public function saveMigrations(array $migrations)
+    {
+        $str = implode(",", array_map(fn ($m) => "('$m')", $migrations));
+
+        $statememnt = $this->pdo->prepare("INSERT INTO migrations (migration) VALUES $str ");
+
+        $statememnt->execute();
+    }
+
+    protected function  log($message)
+    {
+        echo '[' . date('Y-m-d H:i:s') . ']-' . $message . PHP_EOL;
+    }
+}
---
+++ migrations/m0002_something.php
@@ -0,0 +1,14 @@
+<?php
+
+class m0002_something
+{
+    public function up()
+    {
+        echo "applyng migration";
+    }
+
+    public function down()
+    {
+        echo "down migration";
+    }
+}
